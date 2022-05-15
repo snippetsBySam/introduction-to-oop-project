f706c804990a9ccd013ced63185c8b1deca1641e
Sam <snippetsbysam@outlook.com>
2022-05-15 16:03:58 +0100

    Exercise 5.3 complete

--- oop-intro/Bird.cs
+++ oop-intro/Bird.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 
 namespace oop_intro
 {
-    public abstract class Bird
+    public abstract class Bird : IDance
     {
         public string Name { get; private set; }
         public string Color { get; private set; }
@@ -17,15 +17,26 @@ namespace oop_intro
             Color = color;
         }
 
-        //This method is marked with Abstract keyword
-        //This means that any classes extending on Bird must implement this
-        //Abstract methods don't have a method body
-        public abstract void Speak();
+        public abstract void speak();
 
         public virtual void Fly()
         {
-            //Default implementation
             Console.WriteLine("Hi there! I can fly.");
+        }
+
+        public void Spin()
+        {
+            Console.WriteLine($"{Name} spin!");
+        }
+
+        public void DoTheCaterpillar()
+        {
+            Console.WriteLine($"{Name} do the wriggly woo!");
+        }
+
+        public void Jump()
+        {
+            Console.WriteLine($"{Name} jump in the air!");
         }
     }
 }
