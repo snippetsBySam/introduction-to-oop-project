e45164133ac5c5262b7ded465bd410a091afc500
Sam <snippetsbysam@outlook.com>
2022-05-15 15:00:30 +0100

    Exercise 2.5 complete

--- oop-intro/Program.cs
+++ oop-intro/Program.cs
@@ -1,2 +1,5 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using oop_intro;
+
+Bird myBird = new Bird("Tweety", "Yellow");
+myBird.Speak();
+myBird.Fly();
