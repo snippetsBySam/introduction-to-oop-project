48f2f9bbddea6eb2fca67ab115369f0888099d43
Sam <snippetsbysam@outlook.com>
2022-05-15 15:18:50 +0100

    Exercise 4.4 complete

--- oop-intro/Pigeon.cs
+++ oop-intro/Pigeon.cs
@@ -15,6 +15,11 @@ namespace oop_intro
         {
         }
 
+        public override void Speak()
+        {
+            Console.WriteLine($"Yo! I'm {Name} and I'm a {Color} pigeon.");
+        }
+
         public void EatPizza()
         {
             Console.WriteLine("Delicious pizza!");
