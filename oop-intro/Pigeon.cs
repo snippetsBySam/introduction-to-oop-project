40724a17150767ab32ea2ef416c006af6d9243aa
Sam <snippetsbysam@outlook.com>
2022-05-15 15:02:35 +0100

    Exercise 3.3 complete

---
+++ oop-intro/Pigeon.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace oop_intro
+{
+    //The Pigeon extends the base class Bird
+    public class Pigeon : Bird
+    {
+        //The Pigeon constructor calls the base class Bird constructor
+        //The name and color parameters are passed to the Bird constructor
+        public Pigeon(string name, string color) : base(name, color)
+        {
+        }
+
+        public void EatPizza()
+        {
+            Console.WriteLine("Delicious pizza!");
+        }
+    }
+}
